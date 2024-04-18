8ef92eb70d460b42d6cada138daee07510e12b9c
KevinOrosco <kevinorosco411@gmail.com>
2024-04-18 12:07:20 -0300

    Agregue el archivo Murder_Mistery y como solucionarlo

---
+++ crime_scene_report.sql
@@ -0,0 +1,30 @@
+1_  
+SELECT * FROM crime_scene_report;
+WHERE type = 'murder'
+AND city = 'SQL City'
+
+2_
+SELECT * FROM person
+WHERE address_street_name = 'Northwestern Dr'
+ORDER BY address_number DESC;
+
+3_
+SELECT * FROM interview
+WHERE person_id = 14887
+
+4_
+SELECT * FROM get_fit_now_member
+WHERE membership_status = 'gold'
+
+5_
+SELECT * FROM person
+WHERE id = 67318
+
+6_
+SELECT * FROM drivers_license
+WHERE id = 423327
+
+Fimish_
+INSERT INTO solution VALUES (1, 'Jeremy Bowers');
+        
+        SELECT value FROM solution;
